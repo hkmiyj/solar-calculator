d0dfeb6a0d55f3ddf2dec6afff387f473b8b46a1
Hakimi <SCAHBJMUH@slake.sssb.com>
2025-02-21 20:25:04 +0800

    first commit

---
+++ web-ui/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CalculatorComponent } from './pages/solar-calculator/calculator/calculator.component';
+
+const routes: Routes = [
+  {path: 'home', component:HomeComponent},
+  {path: '', component:CalculatorComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ web-ui/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './pages/home/home.component';
+import { CalculatorComponent } from './pages/solar-calculator/calculator/calculator.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CalculatorComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule, 
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
